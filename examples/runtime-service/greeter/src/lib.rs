75a03e7f6c7ed88861833717d42978a648d01623
markfisher <mark@modulewise.com>
2026-03-08 19:09:20 -0400

    runtime-service example
    Signed-off-by: markfisher <mark@modulewise.com>

---
+++ examples/runtime-service/greeter/src/lib.rs
@@ -0,0 +1,18 @@
+#![no_main]
+
+wit_bindgen::generate!({
+    path: "../wit/greeter.wit",
+    world: "greeter",
+    generate_all
+});
+
+struct Greeter;
+
+impl Guest for Greeter {
+    fn greet(name: String) -> String {
+        let greeting = example::greeter::host_greeting::get_greeting();
+        format!("{greeting}, {name}!")
+    }
+}
+
+export!(Greeter);
